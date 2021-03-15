fcca9ff9f2408332a85dfb634ad9dae942b4d3ef
enis <ensozby32@gmail.com>
2021-03-16 00:40:35 +0300

    kahramani nesneye yonelik yapma

--- js/character/iceman/icemanCreate.js
+++ js/character/iceman/icemanCreate.js
@@ -1,3 +1,30 @@
+class Iceman {
+	constructor(xpoint, ypoint, number) {
+		this.xpoint = xpoint;
+		this.ypoint = ypoint;
+		this.number = number;
+		
+	}
+
+	moveLeft() {
+		
+	}
+
+	hops(x, y) {
+		if (x>this.xpoint.start &&
+		    x<this.xpoint.end &&
+		    y>this.ypoint.start &&
+		    y<this.ypoint.end
+		   ) {
+		   	console.log(this.number);
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+
 var imgIceman = document.createElement("img");
 imgIceman.setAttribute("id", "imgIceman");
 imgIceman.setAttribute("src", "img/character/iceman/iceman_wall.png");
@@ -6,7 +33,6 @@ imgIceman.setAttribute("height", "50");
 imgIceman.setAttribute("onclick", "imageSelect(this)");
 
 
-/* hero is in the map */
 var imgIcemanSpriteLeft = document.createElement("img");
 imgIcemanSpriteLeft.setAttribute("id", "imgIcemanSpriteLeft");
 imgIcemanSpriteLeft.setAttribute("src", "img/character/iceman/iceman_map_in_left.png");
@@ -17,4 +43,29 @@ imgIcemanSpriteLeft.setAttribute("ww", "10");
 imgIcemanSpriteLeft.setAttribute("area", "75");
 
 
+/* add the hero to the wall */
 wall.appendChild(imgIceman);
+
+
+
+
+//iceman.imgIceman.setAttribute("onclick", iceman.moveLeft());
+
+/*
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age() {
+    let date = new Date();
+    return date.getFullYear() - this.year;
+  }
+}
+
+let myCar = new Car("Ford", 2014);
+document.getElementById("demo").innerHTML =
+"My car is " + myCar.age() + " years old.";
+*/
+
+
---
+++ js/common/listener.js
@@ -0,0 +1,26 @@
+/* map in keypress listen */
+window.addEventListener("keydown", function (event) {
+	/* leave the hero */
+	if (event.key === "Escape") {
+        globalHeroImageSelected = null;
+        console.log(icemans)
+
+    } else {
+    	//console.log(mapOnTheHeroes);
+    }
+});
+
+
+/* map in click listen */
+window.addEventListener("click", function (event) {
+	const rect = canvas.getBoundingClientRect();
+	const x = event.clientX - rect.left;
+	const y = event.clientY - rect.top;
+	
+	for (iceman in icemans) {
+		if (icemans[iceman].hops(event.offsetX, event.offsetY)) {
+			break;
+		}
+	}
+	
+});
